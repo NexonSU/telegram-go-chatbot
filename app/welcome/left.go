fa03855a23b1b5e2646efc32d3396413222790c0
NexonSU <nexonrievsky@gmail.com>
2021-07-30 04:21:44 +0300

    Ban left users in pending sequence

--- app/welcome/left.go
+++ app/welcome/left.go
@@ -1,6 +1,8 @@
 package welcome
 
 import (
+	"time"
+
 	"github.com/NexonSU/telegram-go-chatbot/app/utils"
 	tb "gopkg.in/tucnak/telebot.v2"
 )
@@ -10,6 +12,17 @@ func OnLeft(m *tb.Message) {
 		return
 	}
 	err := utils.Bot.Delete(m)
+	for i, user := range Border.Users {
+		if user.User.ID == m.Sender.ID && user.Status == "pending" {
+			err := utils.Bot.Ban(Border.Chat, &tb.ChatMember{User: user.User, RestrictedUntil: time.Now().Unix() + 7200})
+			if err != nil {
+				continue
+			}
+			Border.Users[i].Status = "banned"
+			Border.Users[i].Reason = "сбежал"
+			Border.NeedUpdate = true
+		}
+	}
 	if err != nil {
 		utils.ErrorReporting(err, m)
 		return
