a87eb8046c8aa93f06086b2dacb2a75860772479
NexonSU <nexonrievsky@gmail.com>
2021-07-30 05:13:25 +0300

    /getid improvement

--- app/commands/getid.go
+++ app/commands/getid.go
@@ -2,6 +2,8 @@ package commands
 
 import (
 	"fmt"
+	"strings"
+
 	"github.com/NexonSU/telegram-go-chatbot/app/utils"
 	tb "gopkg.in/tucnak/telebot.v2"
 )
@@ -19,6 +21,7 @@ func Getid(m *tb.Message) {
 		}
 		return
 	}
+	var text = strings.Split(m.Text, " ")
 	if m.ReplyTo != nil && m.ReplyTo.OriginalSender != nil {
 		_, err := utils.Bot.Send(m.Sender, fmt.Sprintf("Firstname: %v\nLastname: %v\nUsername: %v\nUserID: %v", m.ReplyTo.OriginalSender.FirstName, m.ReplyTo.OriginalSender.LastName, m.ReplyTo.OriginalSender.Username, m.ReplyTo.OriginalSender.ID))
 		if err != nil {
@@ -27,6 +30,21 @@ func Getid(m *tb.Message) {
 		}
 	} else if m.ReplyTo != nil {
 		_, err := utils.Bot.Send(m.Sender, fmt.Sprintf("Firstname: %v\nLastname: %v\nUsername: %v\nUserID: %v", m.ReplyTo.Sender.FirstName, m.ReplyTo.Sender.LastName, m.ReplyTo.Sender.Username, m.ReplyTo.Sender.ID))
+		if err != nil {
+			utils.ErrorReporting(err, m)
+			return
+		}
+	} else if len(text) == 2 {
+		target, _, err := utils.FindUserInMessage(*m)
+		if err != nil {
+			_, err := utils.Bot.Reply(m, fmt.Sprintf("Не удалось определить пользователя:\n<code>%v</code>", err.Error()))
+			if err != nil {
+				utils.ErrorReporting(err, m)
+				return
+			}
+			return
+		}
+		_, err = utils.Bot.Send(m.Sender, fmt.Sprintf("Firstname: %v\nLastname: %v\nUsername: %v\nUserID: %v", target.FirstName, target.LastName, target.Username, target.ID))
 		if err != nil {
 			utils.ErrorReporting(err, m)
 			return
