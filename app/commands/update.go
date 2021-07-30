008430e7d546f467e5269198f8056c4d229adb87
NexonSU <nexonrievsky@gmail.com>
2021-07-30 05:27:53 +0300

    Add /update command

---
+++ app/commands/update.go
@@ -0,0 +1,33 @@
+package commands
+
+import (
+	"fmt"
+	"os/exec"
+
+	"github.com/NexonSU/telegram-go-chatbot/app/utils"
+	tb "gopkg.in/tucnak/telebot.v2"
+)
+
+//Restart bot on /restart
+func Update(m *tb.Message) {
+	if !utils.IsAdminOrModer(m.Sender.Username) {
+		if m.Chat.Username != utils.Config.Telegram.Chat {
+			return
+		}
+		_, err := utils.Bot.Reply(m, &tb.Animation{File: tb.File{FileID: "CgACAgIAAx0CQvXPNQABHGrDYIBIvDLiVV6ZMPypWMi_NVDkoFQAAq4LAAIwqQlIQT82LRwIpmoeBA"}})
+		if err != nil {
+			utils.ErrorReporting(err, m)
+			return
+		}
+		return
+	}
+	utils.Bot.Delete(m)
+	out, err := exec.Command("go get -u -v github.com/NexonSU/telegram-go-chatbot").Output()
+	if err != nil {
+		utils.ErrorReporting(err, m)
+	}
+	_, err = utils.Bot.Reply(m, fmt.Sprintf("Update finished:\n<pre>%v</pre>", out))
+	if err != nil {
+		utils.ErrorReporting(err, m)
+	}
+}
