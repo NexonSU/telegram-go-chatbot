10193a7a1a46445f905c5b81baa3b9fd845d7975
NexonSU <nexonrievsky@gmail.com>
2021-07-31 06:17:09 +0300

    /ban return fix

--- app/commands/ban.go
+++ app/commands/ban.go
@@ -15,30 +15,24 @@ func Ban(context telebot.Context) error {
 		if context.Chat().Username != utils.Config.Telegram.Chat {
 			return err
 		}
-		err := context.Reply(&telebot.Animation{File: telebot.File{FileID: "CgACAgIAAx0CQvXPNQABHGrDYIBIvDLiVV6ZMPypWMi_NVDkoFQAAq4LAAIwqQlIQT82LRwIpmoeBA"}})
-		return err
+		return context.Reply(&telebot.Animation{File: telebot.File{FileID: "CgACAgIAAx0CQvXPNQABHGrDYIBIvDLiVV6ZMPypWMi_NVDkoFQAAq4LAAIwqQlIQT82LRwIpmoeBA"}})
 	}
 	var text = strings.Split(context.Text(), " ")
 	if (context.Message().ReplyTo == nil && len(text) < 2) || (context.Message().ReplyTo != nil && len(text) > 2) {
-		err := context.Reply("Пример использования: <code>/ban {ID или никнейм}</code>\nИли отправь в ответ на какое-либо сообщение <code>/ban</code>\nЕсли нужно забанить на время, то добавь время в секундах через пробел.")
-		return err
+		return context.Reply("Пример использования: <code>/ban {ID или никнейм}</code>\nИли отправь в ответ на какое-либо сообщение <code>/ban</code>\nЕсли нужно забанить на время, то добавь время в секундах через пробел.")
 	}
 	target, untildate, err := utils.FindUserInMessage(context)
 	if err != nil {
-		err := context.Reply(fmt.Sprintf("Не удалось определить пользователя или время бана:\n<code>%v</code>", err.Error()))
-		return err
+		return context.Reply(fmt.Sprintf("Не удалось определить пользователя или время бана:\n<code>%v</code>", err.Error()))
 	}
 	TargetChatMember, err := utils.Bot.ChatMemberOf(context.Chat(), &target)
 	if err != nil {
-		err := context.Reply(fmt.Sprintf("Ошибка определения пользователя чата:\n<code>%v</code>", err.Error()))
-		return err
+		return context.Reply(fmt.Sprintf("Ошибка определения пользователя чата:\n<code>%v</code>", err.Error()))
 	}
 	TargetChatMember.RestrictedUntil = untildate
 	err = utils.Bot.Ban(context.Chat(), TargetChatMember)
 	if err != nil {
-		err := context.Reply(fmt.Sprintf("Ошибка бана пользователя:\n<code>%v</code>", err.Error()))
-		return err
+		return context.Reply(fmt.Sprintf("Ошибка бана пользователя:\n<code>%v</code>", err.Error()))
 	}
-	err = context.Reply(fmt.Sprintf("Пользователь <a href=\"tg://user?id=%v\">%v</a> забанен%v.", target.ID, utils.UserFullName(&target), utils.RestrictionTimeMessage(untildate)))
-	return err
+	return context.Reply(fmt.Sprintf("Пользователь <a href=\"tg://user?id=%v\">%v</a> забанен%v.", target.ID, utils.UserFullName(&target), utils.RestrictionTimeMessage(untildate)))
 }
