f5421c7430e635999762e42a86ef8d2c890117de
NexonSU <root@nexon.su>
2021-09-27 09:13:53 +0300

    Move middleware to utils

---
+++ app/utils/middleware.go
@@ -0,0 +1,160 @@
+package utils
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/NexonSU/telebot"
+)
+
+func ChatOnly(next telebot.HandlerFunc) telebot.HandlerFunc {
+	return func(context telebot.Context) error {
+		if Config.Chat == context.Chat().Username {
+			return next(context)
+		}
+		return nil
+	}
+}
+
+func ChannelOnly(next telebot.HandlerFunc) telebot.HandlerFunc {
+	return func(context telebot.Context) error {
+		if Config.Channel == context.Chat().Username {
+			return next(context)
+		}
+		return nil
+	}
+}
+
+func CommentChatOnly(next telebot.HandlerFunc) telebot.HandlerFunc {
+	return func(context telebot.Context) error {
+		if Config.CommentChat == context.Chat().ID {
+			return next(context)
+		}
+		return nil
+	}
+}
+
+func GetFilterCreator(next telebot.HandlerFunc) telebot.HandlerFunc {
+	return func(context telebot.Context) error {
+		if Config.SysAdmin == context.Sender().ID {
+			return next(context)
+		}
+		for _, b := range Config.Admins {
+			if b == context.Sender().Username {
+				return next(context)
+			}
+		}
+		for _, b := range Config.Moders {
+			if b == context.Sender().Username {
+				return next(context)
+			}
+		}
+		if Config.Chat == context.Chat().Username {
+			var get Get
+			if context.Message().ReplyTo == nil && len(context.Args()) == 0 {
+				return next(context)
+			}
+			result := DB.Where(&Get{Name: strings.ToLower(context.Args()[0])}).First(&get)
+			if result.RowsAffected != 0 {
+				if get.Creator == context.Sender().ID {
+					return next(context)
+				}
+				creator, err := GetUserFromDB(fmt.Sprintf("%v", get.Creator))
+				if err != nil {
+					return err
+				}
+				return context.Reply(fmt.Sprintf("Данный гет могут изменять либо администраторы, либо %v.", creator.FullName()))
+			}
+			return next(context)
+		}
+		return nil
+	}
+}
+
+func SysLevel(next telebot.HandlerFunc) telebot.HandlerFunc {
+	return func(context telebot.Context) error {
+		if Config.SysAdmin == context.Sender().ID {
+			return next(context)
+		}
+		for _, b := range Config.Admins {
+			if b == context.Sender().Username {
+				return context.Reply(&telebot.Animation{File: telebot.File{FileID: "CgACAgIAAx0CQvXPNQABHGrDYIBIvDLiVV6ZMPypWMi_NVDkoFQAAq4LAAIwqQlIQT82LRwIpmoeBA"}})
+			}
+		}
+		for _, b := range Config.Moders {
+			if b == context.Sender().Username {
+				return context.Reply(&telebot.Animation{File: telebot.File{FileID: "CgACAgIAAx0CQvXPNQABHGrDYIBIvDLiVV6ZMPypWMi_NVDkoFQAAq4LAAIwqQlIQT82LRwIpmoeBA"}})
+			}
+		}
+		if Config.Chat == context.Chat().Username {
+			return context.Reply(&telebot.Animation{File: telebot.File{FileID: "CgACAgIAAx0CQvXPNQABHGrDYIBIvDLiVV6ZMPypWMi_NVDkoFQAAq4LAAIwqQlIQT82LRwIpmoeBA"}})
+		}
+		return nil
+	}
+}
+
+func AdminLevel(next telebot.HandlerFunc) telebot.HandlerFunc {
+	return func(context telebot.Context) error {
+		if Config.SysAdmin == context.Sender().ID {
+			return next(context)
+		}
+		for _, b := range Config.Admins {
+			if b == context.Sender().Username {
+				return next(context)
+			}
+		}
+		for _, b := range Config.Moders {
+			if b == context.Sender().Username {
+				return context.Reply(&telebot.Animation{File: telebot.File{FileID: "CgACAgIAAx0CQvXPNQABHGrDYIBIvDLiVV6ZMPypWMi_NVDkoFQAAq4LAAIwqQlIQT82LRwIpmoeBA"}})
+			}
+		}
+		if Config.Chat == context.Chat().Username {
+			return context.Reply(&telebot.Animation{File: telebot.File{FileID: "CgACAgIAAx0CQvXPNQABHGrDYIBIvDLiVV6ZMPypWMi_NVDkoFQAAq4LAAIwqQlIQT82LRwIpmoeBA"}})
+		}
+		return nil
+	}
+}
+
+func ModerLevel(next telebot.HandlerFunc) telebot.HandlerFunc {
+	return func(context telebot.Context) error {
+		if Config.SysAdmin == context.Sender().ID {
+			return next(context)
+		}
+		for _, b := range Config.Admins {
+			if b == context.Sender().Username {
+				return next(context)
+			}
+		}
+		for _, b := range Config.Moders {
+			if b == context.Sender().Username {
+				return next(context)
+			}
+		}
+		if Config.Chat == context.Chat().Username {
+			return context.Reply(&telebot.Animation{File: telebot.File{FileID: "CgACAgIAAx0CQvXPNQABHGrDYIBIvDLiVV6ZMPypWMi_NVDkoFQAAq4LAAIwqQlIQT82LRwIpmoeBA"}})
+		}
+		return nil
+	}
+}
+
+func ChatLevel(next telebot.HandlerFunc) telebot.HandlerFunc {
+	return func(context telebot.Context) error {
+		if Config.SysAdmin == context.Sender().ID {
+			return next(context)
+		}
+		for _, b := range Config.Admins {
+			if b == context.Sender().Username {
+				return next(context)
+			}
+		}
+		for _, b := range Config.Moders {
+			if b == context.Sender().Username {
+				return next(context)
+			}
+		}
+		if Config.Chat == context.Chat().Username {
+			return next(context)
+		}
+		return nil
+	}
+}
