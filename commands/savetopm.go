16c61797f979b84bd4a673ee17d1b5aa914d5299
NexonSU <root@nexon.su>
2022-04-12 19:34:02 +0300

    /savetopm workaround

--- commands/savetopm.go
+++ commands/savetopm.go
@@ -12,7 +12,7 @@ func SaveToPM(context tele.Context) error {
 	if context.Message() == nil || context.Message().ReplyTo == nil {
 		return context.Reply("Пример использования:\n/savetopm в ответ на какое-либо сообщение")
 	}
-	_, err := utils.Bot.Forward(context.Sender(), context.Message().ReplyTo)
+	_, err := utils.Bot.Copy(context.Sender(), context.Message().ReplyTo)
 	if err != nil {
 		return context.Reply(fmt.Sprintf("Не удалось отправить сообщение в ЛС:\n<code>%v</code>", err.Error()))
 	}
