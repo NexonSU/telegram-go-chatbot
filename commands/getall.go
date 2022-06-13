97e686e152b49bdf6d977ee283443d8b886f774e
NexonSU <root@nexon.su>
2022-06-13 20:41:43 +0300

    /getall fix

--- commands/getall.go
+++ commands/getall.go
@@ -1,25 +1,31 @@
 package commands
 
 import (
-	"fmt"
-	"strings"
-
 	"github.com/NexonSU/telegram-go-chatbot/utils"
 	tele "gopkg.in/telebot.v3"
 )
 
 //Send list of Gets to user on /getall
 func Getall(context tele.Context) error {
-	var getall []string
+	var getall string
 	var get utils.Get
 	result, _ := utils.DB.Model(&utils.Get{}).Rows()
 	for result.Next() {
+		if getall == "" {
+			getall = "Доступные геты: "
+		} else {
+			getall += ", "
+		}
 		err := utils.DB.ScanRows(result, &get)
 		if err != nil {
 			return err
 		}
-		getall = append(getall, get.Name)
+		getall += get.Name
+		if len([]rune(getall)) > 4000 {
+			utils.Bot.Send(context.Sender(), getall)
+			getall = ""
+		}
 	}
-	utils.Bot.Send(context.Sender(), fmt.Sprintf("Доступные геты: %v", strings.Join(getall[:], ", ")))
+	utils.Bot.Send(context.Sender(), getall)
 	return context.Reply("Список отправлен в личку.\nЕсли список не пришел, то убедитесь, что бот запущен и не заблокирован в личке.")
 }
