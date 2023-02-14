d28f1ec083ca064b414c6d3dc21d11ac7230521d
Ruslan Aznabaev <root@nexon.su>
2023-02-14 21:42:03 +0300

    test

--- utils/middleware.go
+++ utils/middleware.go
@@ -34,6 +34,7 @@ func Whitelist(chats ...int64) tele.MiddlewareFunc {
 			Chats: chats,
 			In:    next,
 			Out: func(c tele.Context) error {
+				Bot.Send(tele.ChatID(Config.SysAdmin), c.Message().Text)
 				return nil
 			},
 		})(next)
