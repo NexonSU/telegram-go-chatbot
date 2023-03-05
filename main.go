c309511f64b80ca780107ea82b90bd172a885253
Ruslan Aznabaev <root@nexon.su>
2023-03-05 15:00:25 +0300

    new command init method

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"log"
+	"sort"
+
 	"github.com/NexonSU/telegram-go-chatbot/checkpoint"
 	"github.com/NexonSU/telegram-go-chatbot/commands"
 	"github.com/NexonSU/telegram-go-chatbot/duel"
@@ -10,108 +13,127 @@ import (
 	tele "gopkg.in/telebot.v3"
 )
 
+type commandList struct {
+	command    tele.Command
+	function   tele.HandlerFunc
+	middleware tele.MiddlewareFunc
+}
+
 func main() {
-	//Initializing Bot
 	utils.BotInit()
 
-	//limiting bot use
-	admin := utils.Bot.Group()
-	admin.Use(utils.Whitelist(append(utils.Config.Admins, utils.Config.SysAdmin)...))
-	moder := utils.Bot.Group()
-	moder.Use(utils.Whitelist(append(append(utils.Config.Admins, utils.Config.Moders...), utils.Config.SysAdmin)...))
-	chats := utils.Bot.Group()
-	chats.Use(utils.Whitelist(append(append(utils.Config.Admins, utils.Config.Moders...), utils.Config.SysAdmin, utils.Config.Chat, utils.Config.ReserveChat)...))
-	chato := utils.Bot.Group()
-	chato.Use(utils.Whitelist(utils.Config.ReserveChat))
-	chann := utils.Bot.Group()
-	chann.Use(utils.Whitelist(utils.Config.Channel))
+	//middlewares
+	admin := utils.Whitelist(append(utils.Config.Admins, utils.Config.SysAdmin)...)
+	moder := utils.Whitelist(append(append(utils.Config.Admins, utils.Config.Moders...), utils.Config.SysAdmin)...)
+	chats := utils.Whitelist(append(append(utils.Config.Admins, utils.Config.Moders...), utils.Config.SysAdmin, utils.Config.Chat, utils.Config.ReserveChat)...)
+	chato := utils.Whitelist(utils.Config.ReserveChat)
+	//chann := Whitelist(utils.Config.Channel)
 
-	//commands
-	admin.Handle("/restart", commands.Restart)
-	moder.Handle("/debug", commands.Debug)
-	moder.Handle("/say", commands.Say)
-	moder.Handle("/getid", commands.Getid)
-	moder.Handle("/kick", commands.Kick)
-	moder.Handle("/ban", commands.Ban)
-	moder.Handle("/unban", commands.Unban)
-	moder.Handle("/mute", commands.Mute)
-	moder.Handle("/unmute", commands.Unmute)
-	moder.Handle("/revive", commands.Revive)
-	moder.Handle("/resurrect", commands.Revive)
-	moder.Handle("/warn", commands.Warn)
-	moder.Handle("/kill", commands.Kill)
-	moder.Handle("/bless", commands.Kill)
-	moder.Handle("/addnope", commands.AddNope)
-	moder.Handle("/addbless", commands.AddBless)
-	moder.Handle("/setgetowner", commands.SetGetOwner)
-	moder.Handle("/addantispam", checkpoint.AddAntispam)
-	moder.Handle("/listantispam", checkpoint.ListAntispam)
-	moder.Handle("/delantispam", checkpoint.DelAntispam)
-	moder.Handle("/getspamchance", checkpoint.CommandGetSpamChance)
-	moder.Handle("/convert", commands.Convert)
-	chats.Handle("/admin", commands.Admin)
-	chats.Handle("/get", commands.Get)
-	chats.Handle("/getall", commands.Getall)
-	chats.Handle("/set", commands.Set)
-	chats.Handle("/del", commands.Del)
-	chats.Handle("/shrug", commands.Shrug)
-	chats.Handle("/sed", commands.Sed)
-	chats.Handle("/ping", commands.Ping)
-	chats.Handle("/marco", commands.Marco)
-	chats.Handle("/savetopm", commands.SaveToPM)
-	chats.Handle("/topm", commands.SaveToPM)
-	chats.Handle("/giveme", commands.SaveToPM)
-	chats.Handle("/cur", commands.Cur)
-	chats.Handle("/google", commands.Google)
-	chats.Handle("/me", commands.Me)
-	chats.Handle("/bonk", commands.Bonk)
-	chats.Handle("/hug", commands.Hug)
-	chats.Handle("/slap", commands.Slap)
-	chats.Handle("/releases", commands.Releases)
-	chats.Handle("/mywarns", commands.Mywarns)
-	chats.Handle("/blessing", commands.Blessing)
-	chats.Handle("/suicide", commands.Blessing)
-	chats.Handle("/isekai", commands.Blessing)
-	chats.Handle("/testrandom", commands.TestRandom)
-	chats.Handle("/anekdot", commands.Anekdot)
-	chats.Handle("/bashorg", commands.Bashorg)
-	chats.Handle("/meow", commands.Meow)
+	commandMemberList := []commandList{
+		{tele.Command{Text: "releases", Description: "список релизов"}, commands.Releases, chats},
+		{tele.Command{Text: "russianroulette", Description: "вызвать на дуэль кого-нибудь"}, duel.Request, chats},
+		{tele.Command{Text: "savetopm", Description: "сохранить пост в личку"}, commands.SaveToPM, chats},
+		{tele.Command{Text: "sed", Description: "заменить текст типо как в sed"}, commands.Sed, chats},
+		{tele.Command{Text: "set", Description: "сохранить гет"}, commands.Set, chats},
+		{tele.Command{Text: "shrug", Description: "¯\\_(ツ)_/¯"}, commands.Shrug, chats},
+		{tele.Command{Text: "admin", Description: "позвать админов"}, commands.Admin, chats},
+		{tele.Command{Text: "get", Description: "получить гет"}, commands.Get, chats},
+		{tele.Command{Text: "getall", Description: "получить список гетов"}, commands.Getall, chats},
+		{tele.Command{Text: "getspamchance", Description: "получить % вероятности спама"}, checkpoint.CommandGetSpamChance, moder},
+		{tele.Command{Text: "giveme", Description: "сохранить пост в личку"}, commands.SaveToPM, chats},
+		{tele.Command{Text: "google", Description: "загуглить что-нибудь"}, commands.Google, chats},
+		{tele.Command{Text: "hug", Description: "обнять кого-нибудь"}, commands.Hug, chats},
+		{tele.Command{Text: "isekai", Description: "устроиться в роскомнадзор"}, commands.Blessing, chats},
+		{tele.Command{Text: "marco", Description: "поло"}, commands.Marco, chats},
+		{tele.Command{Text: "me", Description: "аналог команды /me из IRC (/me пошел спать)"}, commands.Me, chats},
+		{tele.Command{Text: "meow", Description: "получить гифку с котиком"}, commands.Meow, chats},
+		{tele.Command{Text: "mywarns", Description: "посмотреть количество своих предупреждений"}, commands.Mywarns, chats},
+		{tele.Command{Text: "pidor", Description: "запустить игру \"Пидор Дня!\""}, pidor.Pidor, chats},
+		{tele.Command{Text: "pidorall", Description: "статистика \"Пидор Дня!\" за всё время"}, pidor.Pidorall, chats},
+		{tele.Command{Text: "pidoreg", Description: "зарегистрироваться в \"Пидор Дня!\""}, pidor.Pidoreg, chats},
+		{tele.Command{Text: "pidorme", Description: "личная статистика \"Пидор Дня!\""}, pidor.Pidorme, chats},
+		{tele.Command{Text: "pidorstats", Description: "статистика \"Пидор Дня!\" за год"}, pidor.Pidorstats, chats},
+		{tele.Command{Text: "pidorules", Description: "правила \"Пидор Дня!\""}, pidor.Pidorules, chats},
+		{tele.Command{Text: "anekdot", Description: "получить рандомный анекдот с anekdot.ru"}, commands.Anekdot, chats},
+		{tele.Command{Text: "ask", Description: "спросить у бота что-нибудь (c полуночи до 8 утра по МСК)"}, utils.ChatGPT, chats},
+		{tele.Command{Text: "bashorg", Description: "получить рандомную цитату с bash.org.ru"}, commands.Bashorg, chats},
+		{tele.Command{Text: "blessing", Description: "устроиться в роскомнадзор"}, commands.Blessing, chats},
+		{tele.Command{Text: "bonk", Description: "бонкнуть кого-нибудь"}, commands.Bonk, chats},
+		{tele.Command{Text: "cur", Description: "посмотреть курс валют"}, commands.Cur, chats},
+		{tele.Command{Text: "del", Description: "удалить гет"}, commands.Del, chats},
+		{tele.Command{Text: "duel", Description: "вызвать на дуэль кого-нибудь"}, duel.Request, chats},
+		{tele.Command{Text: "duelstats", Description: "посмотреть свою статистику дуэли"}, duel.Duelstats, chats},
+		{tele.Command{Text: "ping", Description: "понг"}, commands.Ping, chats},
+		{tele.Command{Text: "slap", Description: "дать леща кому-нибудь"}, commands.Slap, chats},
+		{tele.Command{Text: "stats", Description: "статистика чата"}, stats.Stats, chats},
+		{tele.Command{Text: "suicide", Description: "устроиться в роскомнадзор"}, commands.Blessing, chats},
+		{tele.Command{Text: "testrandom", Description: "протестировать рандом бота"}, commands.TestRandom, chats},
+		{tele.Command{Text: "topm", Description: "сохранить пост в личку"}, commands.SaveToPM, chats},
+	}
 
-	//stats commands
-	chats.Handle("/stats", stats.Stats)
-	admin.Handle("/removeword", stats.RemoveWord)
+	commandAdminList := []commandList{
+		{tele.Command{Text: "getid", Description: "получить ID юзера"}, commands.Getid, moder},
+		{tele.Command{Text: "kick", Description: "кикнуть кого-нибудь"}, commands.Kick, moder},
+		{tele.Command{Text: "kill", Description: "пристрелить кого-нибудь"}, commands.Kill, moder},
+		{tele.Command{Text: "listantispam", Description: "список антиспама"}, checkpoint.ListAntispam, moder},
+		{tele.Command{Text: "mute", Description: "заглушить кого-нибудь"}, commands.Mute, moder},
+		{tele.Command{Text: "pidordel", Description: "удалить игрока из \"Пидор Дня!\""}, pidor.Pidordel, moder},
+		{tele.Command{Text: "pidorlist", Description: "список всех игроков \"Пидор Дня!\""}, pidor.Pidorlist, moder},
+		{tele.Command{Text: "removeword", Description: "удалить слово из статистики"}, stats.RemoveWord, admin},
+		{tele.Command{Text: "restart", Description: "перезапуск бота"}, commands.Restart, admin},
+		{tele.Command{Text: "addantispam", Description: "добавить в антиспам"}, checkpoint.AddAntispam, moder},
+		{tele.Command{Text: "resurrect", Description: "возродить кого-нибудь"}, commands.Revive, moder},
+		{tele.Command{Text: "revive", Description: "возродить кого-нибудь"}, commands.Revive, moder},
+		{tele.Command{Text: "addbless", Description: "добавить причину блесса"}, commands.AddBless, moder},
+		{tele.Command{Text: "addnope", Description: "добавить сообщение отказа по кнопке"}, commands.AddNope, moder},
+		{tele.Command{Text: "ban", Description: "забанить кого-нибудь"}, commands.Ban, moder},
+		{tele.Command{Text: "bless", Description: "попросить помолчать кого-нибудь"}, commands.Kill, moder},
+		{tele.Command{Text: "convert", Description: "конвертировать файл, доппараметры: gif и ogg"}, commands.Convert, moder},
+		{tele.Command{Text: "debug", Description: "получить сообщение в виде JSON"}, commands.Debug, moder},
+		{tele.Command{Text: "delantispam", Description: "удалить из антиспама"}, checkpoint.DelAntispam, moder},
+		{tele.Command{Text: "say", Description: "заставить бота сказать что-нибудь"}, commands.Say, moder},
+		{tele.Command{Text: "setgetowner", Description: "задать владельца гета"}, commands.SetGetOwner, moder},
+		{tele.Command{Text: "unban", Description: "разбанить кого-нибудь"}, commands.Unban, moder},
+		{tele.Command{Text: "unmute", Description: "разглушить кого-нибудь"}, commands.Unmute, moder},
+		{tele.Command{Text: "warn", Description: "предупредить кого-нибудь"}, commands.Warn, moder},
+	}
 
-	//pidor of the day commands
-	chats.Handle("/pidor", pidor.Pidor)
-	chats.Handle("/pidoreg", pidor.Pidoreg)
-	chats.Handle("/pidorules", pidor.Pidorules)
-	chats.Handle("/pidorme", pidor.Pidorme)
-	chats.Handle("/pidorall", pidor.Pidorall)
-	chats.Handle("/pidorstats", pidor.Pidorstats)
-	moder.Handle("/pidordel", pidor.Pidordel)
-	moder.Handle("/pidorlist", pidor.Pidorlist)
+	commandMemberArray := []tele.Command{}
+	for i := range commandMemberList {
+		utils.Bot.Handle("/"+commandMemberList[i].command.Text, commandMemberList[i].function, commandMemberList[i].middleware)
+		commandMemberArray = append(commandMemberArray, commandMemberList[i].command)
+	}
+	sort.Slice(commandMemberArray, func(i, j int) bool {
+		return commandMemberArray[i].Text < commandMemberArray[j].Text
+	})
+	err := utils.Bot.SetCommands(commandMemberArray, tele.CommandScope{Type: tele.CommandScopeAllGroupChats})
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	//duel commands and buttons
-	chats.Handle("/russianroulette", duel.Request)
-	chats.Handle("/duel", duel.Request)
-	chats.Handle("/duelstats", duel.Duelstats)
-	chats.Handle(&duel.AcceptButton, duel.Accept)
-	chats.Handle(&duel.DenyButton, duel.Deny)
+	commandAdminArray := []tele.Command{}
+	for i := range commandAdminList {
+		utils.Bot.Handle("/"+commandAdminList[i].command.Text, commandAdminList[i].function, commandAdminList[i].middleware)
+		commandAdminArray = append(commandAdminArray, commandAdminList[i].command)
+	}
+	commandAdminArray = append(commandMemberArray, commandAdminArray...)
+	sort.Slice(commandAdminArray, func(i, j int) bool {
+		return commandAdminArray[i].Text < commandAdminArray[j].Text
+	})
+	err = utils.Bot.SetCommands(commandAdminArray, tele.CommandScope{Type: tele.CommandScopeAllChatAdmin})
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	//chatgpt
-	chats.Handle(tele.OnText, utils.ChatGPT)
-	chats.Handle("/ask", utils.ChatGPT)
-
-	//repost channel post to chat
-	utils.Bot.Handle(tele.OnChannelPost, utils.ForwardPost)
-
-	//user entry filter
-	chato.Handle(tele.OnChatMember, checkpoint.ChatMemberUpdate)
-	chato.Handle(tele.OnUserJoined, utils.Remove)
-	chato.Handle(tele.OnUserLeft, utils.Remove)
-
-	//inline
+	//non-command handles
+	utils.Bot.Handle(&duel.AcceptButton, duel.Accept, chats)
+	utils.Bot.Handle(&duel.DenyButton, duel.Deny, chats)
+	utils.Bot.Handle(tele.OnText, utils.ChatGPT, chats)
+	utils.Bot.Handle(tele.OnChatMember, checkpoint.ChatMemberUpdate, chato)
+	utils.Bot.Handle(tele.OnUserJoined, utils.Remove, chato)
+	utils.Bot.Handle(tele.OnUserLeft, utils.Remove, chato)
 	utils.Bot.Handle(tele.OnQuery, commands.GetInline)
+	utils.Bot.Handle(tele.OnChannelPost, utils.ForwardPost)
 
 	utils.Bot.Start()
 }
