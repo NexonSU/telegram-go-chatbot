4b77c86f1b3c2e82600fe43a569575f2ef26ea36
Ruslan Aznabaev <ruslan.aznabaev@vitech.team>
2023-06-05 15:05:19 +0300

    fix /stats

--- commands/stats.go
+++ commands/stats.go
@@ -6,5 +6,5 @@ import (
 
 // Reply with stats link
 func Stats(context tele.Context) error {
-	return context.Reply("http://t.me/zavtrachat_bot/stats")
+	return context.Reply("<a href='https://t.me/zavtrachat_bot/stats'>Webapp</a>\n<a href='https://grafana.nexon.su/d/aef7a25c-3824-4046-8ed3-53ccb5850c9d/zavtrachat?kiosk'>Прямая ссылка</a>")
 }
