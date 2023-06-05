5d1440fab4e77fbae9f646197cdf9a9682db00f7
Ruslan Aznabaev <ruslan.aznabaev@vitech.team>
2023-06-05 14:56:45 +0300

    /stats

--- commands/stats.go
+++ commands/stats.go
@@ -6,12 +6,5 @@ import (
 
 // Reply with stats link
 func Stats(context tele.Context) error {
-	return context.Reply("Статистика чата", &tele.ReplyMarkup{
-		InlineKeyboard: [][]tele.InlineButton{{
-			{
-				Text:   "Открыть",
-				WebApp: &tele.WebApp{URL: "https://grafana.nexon.su/d/aef7a25c-3824-4046-8ed3-53ccb5850c9d/zavtrachat?orgId=1&kiosk"},
-			},
-		}},
-	})
+	return context.Reply("http://t.me/zavtrachat_bot/stats")
 }
