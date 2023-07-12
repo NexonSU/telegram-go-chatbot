983272428ca05b4eb0672d7d9b75778ab6b9c222
NexonSU <root@nexon.su>
2023-07-12 17:37:16 +0300

    change to uptade

--- utils/bot.go
+++ utils/bot.go
@@ -110,11 +110,9 @@ func ErrorReporting(err error, context tele.Context) {
 	if strings.Contains(err.Error(), "context does not contain message") {
 		return
 	}
-	if context != nil {
-		marshalledContext, _ := json.MarshalIndent(context, "", "    ")
-		marshalledContextWithoutNil := regexp.MustCompile(`.*": (null|""|0|false)(,|)\n`).ReplaceAllString(string(marshalledContext), "")
-		jsonMessage := html.EscapeString(marshalledContextWithoutNil)
-		text += fmt.Sprintf("\n\nMessage:\n<pre>%v</pre>", jsonMessage)
-	}
+	marshalledContext, _ := json.MarshalIndent(context.Update(), "", "    ")
+	marshalledContextWithoutNil := regexp.MustCompile(`.*": (null|""|0|false)(,|)\n`).ReplaceAllString(string(marshalledContext), "")
+	jsonMessage := html.EscapeString(marshalledContextWithoutNil)
+	text += fmt.Sprintf("\n\nMessage:\n<pre>%v</pre>", jsonMessage)
 	Bot.Send(tele.ChatID(Config.SysAdmin), text)
 }
