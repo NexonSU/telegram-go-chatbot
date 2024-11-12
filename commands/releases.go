99c34f337e436d0d167bcc4b5d6c5e07516b68f7
NexonSU <root@nexon.su>
2024-11-12 20:44:52 +0300

    change calendar

--- commands/releases.go
+++ commands/releases.go
@@ -8,18 +8,24 @@ import (
 	"strings"
 	"time"
 
-	"github.com/NexonSU/telegram-go-chatbot/utils"
 	ical "github.com/arran4/golang-ical"
 	tele "gopkg.in/telebot.v3"
 )
 
+func countRune(s string, r rune) int {
+	count := 0
+	for _, c := range s {
+		if c == r {
+			count++
+		}
+	}
+	return count
+}
+
 // Send releases of 2 weeks on /releases
 func Releases(context tele.Context) error {
 	var err error
-	if utils.Config.ReleasesUrl == "" {
-		return utils.ReplyAndRemove("Список ближайших релизов не настроен", context)
-	}
-	resp, err := http.Get(utils.Config.ReleasesUrl)
+	resp, err := http.Get("https://ical-videogames.onrender.com/calendar?platform=ps4&platform=ps5&platform=switch&platform=xbox_one&platform=xbox_series&region=pal")
 	if err != nil {
 		return err
 	}
@@ -28,18 +34,21 @@ func Releases(context tele.Context) error {
 		return err
 	}
 	releases := ""
-	today, _ := strconv.Atoi(time.Now().Format("20060102"))
-	twoweeks, _ := strconv.Atoi(time.Now().AddDate(0, 0, 14).Format("20060102"))
+	twodaysago, _ := strconv.Atoi(time.Now().AddDate(0, 0, -2).Format("20060102"))
 	events := cal.Events()
 	sort.SliceStable(events, func(i, j int) bool {
-		return events[i].GetProperty(ical.ComponentPropertyDtStart).Value > events[j].GetProperty(ical.ComponentPropertyDtStart).Value
+		return events[i].GetProperty(ical.ComponentPropertyDtStart).Value < events[j].GetProperty(ical.ComponentPropertyDtStart).Value
 	})
 	for _, element := range events {
 		date := element.GetProperty(ical.ComponentPropertyDtStart).Value
 		name := element.GetProperty(ical.ComponentPropertySummary).Value
+		name = strings.Split(name, " (")[0]
+		if strings.Contains(releases, name) {
+			continue
+		}
 		dateint, _ := strconv.Atoi(date)
-		if dateint > today && dateint < twoweeks {
-			releases = fmt.Sprintf("<b>%v</b> - %v.%v.%v\n%v", strings.ReplaceAll(name, "\\,", ","), date[6:8], date[4:6], date[0:4], releases)
+		if dateint > twodaysago && countRune(releases, '\n') < 20 {
+			releases = fmt.Sprintf("%v\n%v.%v.%v: %v", releases, date[6:8], date[4:6], date[0:4], name)
 		}
 	}
 	return context.Reply(releases)
